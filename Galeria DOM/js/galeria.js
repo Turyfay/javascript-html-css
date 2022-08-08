2cede26c564e3ecb37b08365b3f5996fdb79e887
David Enamorado <david8enaceituno@gmail.com>
2022-08-07 19:18:09 -0600

    add: Detectar evento click

--- Galeria DOM/js/galeria.js
+++ Galeria DOM/js/galeria.js
@@ -1,3 +1,14 @@
 window.onload = ()=>{
-    
+    const fotos = document.querySelector("div");
+
+    fotos.addEventListener("click",(e)=>{
+        if(e.target.tagName === "IMG"){
+            const imagenMini = e.target.src;
+            const archivo = imagenMini.substr(imagenMini.length - 10,10);
+
+            console.log(archivo);
+        }else{
+            console.log("Lo sentimos, no seleccionastes una imagen");
+        }
+    },false);
 }
