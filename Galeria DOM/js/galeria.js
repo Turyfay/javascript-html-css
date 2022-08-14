f03efdb39a7683ca20735b5570afeb7b9da2816d
David Enamorado <david8enaceituno@gmail.com>
2022-08-13 23:38:36 -0600

    add: carga de imagen grande en el fondo negro

--- Galeria DOM/js/galeria.js
+++ Galeria DOM/js/galeria.js
@@ -3,10 +3,38 @@ window.onload = ()=>{
 
     fotos.addEventListener("click",(e)=>{
         if(e.target.tagName === "IMG"){
+            //Crear fondo negro
+            const fondoNegro = document.createElement("div");
+            fondoNegro.id = "fondo";
+            document.body.appendChild(fondoNegro);
+            //Estilo del fondo negro
+            fondoNegro.style.position = "absolute";
+            fondoNegro.style.top = 0;
+            fondoNegro.style.backgroundColor = "rgba(0,0,0,0.5)";
+            fondoNegro.style.cursor = "pointer";
+
+            //dimensiones 
+            fondoNegro.style.width = window.innerWidth + "px";
+            fondoNegro.style.height = window.innerHeight + "px";
+            fondoNegro.style.top = window.scrollY +"px";
+            fondoNegro.style.left = window.scrollX +"px";
+
+            //Crear imagen
+
+
             const imagenMini = e.target.src;
             const archivo = imagenMini.substr(imagenMini.length - 10,10);
+            const imagenGrande = "img/fondos/"+archivo;
+            const imagen = document.createElement("img");
+            imagen.id = "imagen";
+            imagen.src = imagenGrande;
+            imagen.style.display = "block";
+            imagen.style.position = "absolute";
 
-            console.log(archivo);
+            imagen.addEventListener("load",()=>{
+                fondoNegro.appendChild(imagen);
+            },false);
+
         }else{
             console.log("Lo sentimos, no seleccionastes una imagen");
         }
